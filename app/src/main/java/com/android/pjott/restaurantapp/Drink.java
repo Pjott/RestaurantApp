ad81cd0729b2f266d6c873509cbc3504a9f9377c
Pjott <piotr.baa@gmail.com>
2018-02-19 20:52:30 +0100

    First version of start screen and drinks resources

--- app/src/main/java/com/android/pjott/restaurantapp/Drink.java
+++ app/src/main/java/com/android/pjott/restaurantapp/Drink.java
@@ -1,8 +1,35 @@
 package com.android.pjott.restaurantapp;
 
-/**
- * Created by Piotr on 19.02.2018.
- */
-
 public class Drink {
+    private String drinkName;
+    private String drinkDescription;
+    private int drinkImageResourceId;
+
+    private Drink(String drinkName, String drinkDescription, int drinkImageResourceId) {
+        this.drinkName = drinkName;
+        this.drinkDescription  = drinkDescription;
+        this.drinkImageResourceId = drinkImageResourceId;
+    }
+
+    public static final Drink[] drinks = {
+            new Drink("Coffee", "Coffee with milk", R.drawable.coffee),
+            new Drink("Beer", "A delicious beer", R.drawable.beer),
+            new Drink("Proseco", "Proseco", R.drawable.prosecco)
+    };
+
+    public String getDrinkName() {
+        return drinkName;
+    }
+
+    public String getDrinkDescription() {
+        return drinkDescription;
+    }
+
+    public int getDrinkImageResourceId() {
+        return drinkImageResourceId;
+    }
+
+    public String toString() {
+        return this.drinkName;
+    }
 }
