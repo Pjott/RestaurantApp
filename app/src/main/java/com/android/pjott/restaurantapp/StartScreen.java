725d83a5e33ca1e088086c0fca66d8bb5af92c19
Pjott <piotr.baa@gmail.com>
2018-02-20 20:59:18 +0100

    Reponding on clicking on list view item (Menu)

--- app/src/main/java/com/android/pjott/restaurantapp/StartScreen.java
+++ app/src/main/java/com/android/pjott/restaurantapp/StartScreen.java
@@ -1,7 +1,11 @@
 package com.android.pjott.restaurantapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class StartScreen extends Activity {
 
@@ -9,5 +13,20 @@ public class StartScreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+
+        AdapterView.OnItemClickListener itemClickListener =
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> listView, View itemView, int position, long id) {
+                        if (position == 0) {
+                            Intent startIntent = new Intent(StartScreen.this, Menu.class);
+                            startActivity(startIntent);
+                        }
+                    }
+                };
+
+        ListView startScreenListView = (ListView) findViewById(R.id.main_options_list);
+        startScreenListView.setOnItemClickListener(itemClickListener);
+
     }
 }
