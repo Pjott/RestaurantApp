bb6eee99c178adcc83611c3d16f3ce6468fc28c9
Pjott <piotr.baa@gmail.com>
2018-02-21 20:35:35 +0100

    Updated Menu activity and its view

--- app/src/main/java/com/android/pjott/restaurantapp/Menu.java
+++ app/src/main/java/com/android/pjott/restaurantapp/Menu.java
@@ -1,7 +1,11 @@
 package com.android.pjott.restaurantapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class Menu extends Activity {
 
@@ -9,5 +13,19 @@ public class Menu extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        AdapterView.OnItemClickListener itemClickListener =
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> listView, View itemView, int position, long id) {
+                        if (position == 3) {
+                            Intent menuIntent = new Intent(Menu.this, Drink.class);
+                            startActivity(menuIntent);
+                        }
+                    }
+                };
+
+        ListView menuListView = (ListView) findViewById(R.id.main_options_list);
+        menuListView.setOnItemClickListener(itemClickListener);
     }
 }
