3ec59f3a2b3fc8d03ef0330b0d0fe66a2213c15e
Pjott <piotr.baa@gmail.com>
2018-02-23 21:46:40 +0100

    Passing intent between DrinkCategoryActivity and DrinkActivity (Complete DrinkActivity- showing drink details)

--- app/src/main/java/com/android/pjott/restaurantapp/DrinkActivity.java
+++ app/src/main/java/com/android/pjott/restaurantapp/DrinkActivity.java
@@ -1,13 +1,32 @@
 package com.android.pjott.restaurantapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DrinkActivity extends Activity {
+
+    public static final String EXTRA_DRINKID = "drinkId";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink);
+
+        //Gettig intetn fromDrinkCategoryActivity
+        int drinkId = (Integer) getIntent().getExtras().get(EXTRA_DRINKID);
+        Drink drink = Drink.drinks[drinkId];
+
+        TextView drinkName = (TextView) findViewById(R.id.drink_name);
+        drinkName.setText(drink.getDrinkName());
+
+        TextView drinkDescription = (TextView) findViewById(R.id.drink_description);
+        drinkDescription.setText(drink.getDrinkDescription());
+
+        ImageView drinkPhoto = (ImageView) findViewById(R.id.drink_photo);
+        drinkPhoto.setImageResource(drink.getDrinkImageResourceId());
+        drinkPhoto.setContentDescription(drink.getDrinkName());
     }
 }
--- app/src/main/java/com/android/pjott/restaurantapp/DrinkCategoryActivity.java
+++ app/src/main/java/com/android/pjott/restaurantapp/DrinkCategoryActivity.java
@@ -3,6 +3,8 @@ package com.android.pjott.restaurantapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -13,8 +15,28 @@ public class DrinkCategoryActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_category);
 
-        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Drink.drinks);
+        //Bind data from Drink class
+        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(this,
+                                    android.R.layout.simple_list_item_1,
+                                    Drink.drinks);
         ListView listDrinks = (ListView) findViewById(R.id.drinks_list);
         listDrinks.setAdapter(listAdapter);
+
+        AdapterView.OnItemClickListener itemClickListener =
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> listDrinks,
+                                            View itemView,
+                                            int position,
+                                            long id) {
+                        Intent intentDrinkCategoryActivity = new Intent(DrinkCategoryActivity.this,
+                                DrinkActivity.class);
+                        intentDrinkCategoryActivity
+                                .putExtra(DrinkActivity.EXTRA_DRINKID, (int) id);
+                        startActivity(intentDrinkCategoryActivity);
+                    }
+                };
+
+        listDrinks.setOnItemClickListener(itemClickListener);
     }
 }
