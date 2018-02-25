34492c9103765759ddef0abf55ba51e9344d50da
Pjott <piotr.baa@gmail.com>
2018-02-25 18:14:54 +0100

    Connection between LunchCategoyActivity and LunchActivity

--- app/src/main/java/com/android/pjott/restaurantapp/LunchActivity.java
+++ app/src/main/java/com/android/pjott/restaurantapp/LunchActivity.java
@@ -2,12 +2,29 @@ package com.android.pjott.restaurantapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class LunchActivity extends Activity {
+
+    public static final String EXTRA_LUNCHID = "lunchId";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lunch);
+
+        int lunchId = (Integer) getIntent().getExtras().get(EXTRA_LUNCHID);
+        Lunch lunch = Lunch.lunchArray[lunchId];
+
+        TextView lunchName = (TextView) findViewById(R.id.lunch_name);
+        lunchName.setText(lunch.getLunchName());
+
+        TextView lunchDescription = (TextView) findViewById(R.id.lunch_description);
+        lunchDescription.setText(lunch.getLunchDescription());
+
+        ImageView lunchPhoto = (ImageView) findViewById(R.id.lunch_photo);
+        lunchPhoto.setImageResource(lunch.getLunchImageResourceId());
+        lunchPhoto.setContentDescription(lunch.getLunchName());
     }
 }
--- app/src/main/java/com/android/pjott/restaurantapp/LunchCategoryActivity.java
+++ app/src/main/java/com/android/pjott/restaurantapp/LunchCategoryActivity.java
@@ -1,7 +1,12 @@
 package com.android.pjott.restaurantapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class LunchCategoryActivity extends Activity {
 
@@ -9,5 +14,27 @@ public class LunchCategoryActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lunch_category);
+
+        ArrayAdapter<Lunch> listAdapter = new ArrayAdapter<Lunch>(this,
+                                    android.R.layout.simple_list_item_1,
+                                    Lunch.lunchArray);
+        ListView lunchList = (ListView) findViewById(R.id.lunch_list);
+        lunchList.setAdapter(listAdapter);
+
+        AdapterView.OnItemClickListener itemClickListener =
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> lunchList,
+                                            View view,
+                                            int position,
+                                            long id) {
+                        Intent intentLunchCategoryActivity = new Intent(LunchCategoryActivity.this,
+                                LunchActivity.class);
+                        intentLunchCategoryActivity.putExtra(LunchActivity.EXTRA_LUNCHID, (int) id);
+                        startActivity(intentLunchCategoryActivity);
+                    }
+                };
+
+        lunchList.setOnItemClickListener(itemClickListener);
     }
 }
