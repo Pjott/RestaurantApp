ac8d07be21ddde4a157fa542beb9cbbd00bc8956
Pjott <piotr.baa@gmail.com>
2018-02-25 13:16:21 +0100

    Starting Lunch category delevopment

---
+++ app/src/main/java/com/android/pjott/restaurantapp/LunchCategoryActivity.java
@@ -0,0 +1,13 @@
+package com.android.pjott.restaurantapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LunchCategoryActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lunch_category);
+    }
+}
