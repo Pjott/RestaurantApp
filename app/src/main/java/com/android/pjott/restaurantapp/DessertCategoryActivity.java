bc9e05b98ae5ff484e8a71247480c8f6f12ea174
Pjott <piotr.baa@gmail.com>
2018-02-25 21:12:09 +0100

    Binding data from Dessert class to Dessert category activity

--- app/src/main/java/com/android/pjott/restaurantapp/DessertCategoryActivity.java
+++ app/src/main/java/com/android/pjott/restaurantapp/DessertCategoryActivity.java
@@ -2,6 +2,8 @@ package com.android.pjott.restaurantapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class DessertCategoryActivity extends Activity {
 
@@ -9,5 +11,11 @@ public class DessertCategoryActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dessert_category);
+
+        ArrayAdapter<Dessert> listAdapter = new ArrayAdapter<Dessert>(this,
+                                        android.R.layout.simple_list_item_1,
+                                        Dessert.desserts);
+        ListView dessertsList = (ListView) findViewById(R.id.desserts_list);
+        dessertsList.setAdapter(listAdapter);
     }
 }
