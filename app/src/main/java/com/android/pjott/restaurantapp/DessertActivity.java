fc796931dcdf8cf9b3c0156ad191a3b8e7208fb8
Pjott <piotr.baa@gmail.com>
2018-02-25 22:29:04 +0100

    Connection between DessertActivity and DessertCategoryActivity. Finished DessertActivity view

--- app/src/main/java/com/android/pjott/restaurantapp/DessertActivity.java
+++ app/src/main/java/com/android/pjott/restaurantapp/DessertActivity.java
@@ -2,12 +2,29 @@ package com.android.pjott.restaurantapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DessertActivity extends Activity {
+
+    public static final String EXTRA_DESSERTID = "dessertId";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dessert);
+
+        int dessertId = (Integer) getIntent().getExtras().get(EXTRA_DESSERTID);
+        Dessert dessert = Dessert.desserts[dessertId];
+
+        TextView drinkName = (TextView) findViewById(R.id.dessert_name);
+        drinkName.setText(dessert.getDessertName());
+
+        TextView drinkDescription = (TextView) findViewById(R.id.dessert_description);
+        drinkDescription.setText(dessert.getDessertDescription());
+
+        ImageView drinkPhoto = (ImageView) findViewById(R.id.dessert_photo);
+        drinkPhoto.setImageResource(dessert.getDessertImageResourceId());
+        drinkPhoto.setContentDescription(dessert.getDessertName());
     }
 }
--- app/src/main/java/com/android/pjott/restaurantapp/DessertCategoryActivity.java
+++ app/src/main/java/com/android/pjott/restaurantapp/DessertCategoryActivity.java
@@ -1,7 +1,10 @@
 package com.android.pjott.restaurantapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -17,5 +20,22 @@ public class DessertCategoryActivity extends Activity {
                                         Dessert.desserts);
         ListView dessertsList = (ListView) findViewById(R.id.desserts_list);
         dessertsList.setAdapter(listAdapter);
+
+        AdapterView.OnItemClickListener itemClickListener =
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> listDesserts,
+                                            View listView,
+                                            int position,
+                                            long id) {
+                        Intent intentDessertCategoryActivity = new Intent(DessertCategoryActivity.this,
+                                DessertActivity.class);
+                        intentDessertCategoryActivity
+                                .putExtra(DessertActivity.EXTRA_DESSERTID, (int) id);
+                        startActivity(intentDessertCategoryActivity);
+                    }
+                };
+
+        dessertsList.setOnItemClickListener(itemClickListener);
     }
 }
